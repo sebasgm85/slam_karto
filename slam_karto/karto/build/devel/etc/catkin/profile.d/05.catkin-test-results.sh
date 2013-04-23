5c873e0009549d9ebe16aaf0009af95176a78602
Stephan Wirth <swirth@willowgarage.com>
2013-04-22 23:13:34 -0700

    karto cleanup

--- slam_karto/karto/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.em
-
-export CATKIN_TEST_RESULTS_DIR="/home/sebasgm/ros_devel/src/slam_karto/slam_karto/karto/build/test_results"
-export ROS_TEST_RESULTS_DIR=$CATKIN_TEST_RESULTS_DIR
